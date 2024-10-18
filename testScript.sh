c09b0f16db694d56a2c3286843e5f505dec96ffc
ChanchakornP <gamechanchakorn@gmail.com>
2024-10-18 11:26:24 +1100

    update config files

--- testScript.sh
+++
@@ -1,7 +0,0 @@
-gradle test --tests KeyEventTest jacocoTestReport
-gradle test --tests LogicTest jacocoTestReport
-gradle test --tests MouseEventTest jacocoTestReport
-gradle test --tests StageEndTest jacocoTestReport
-gradle test --tests StageTest jacocoTestReport
-gradle test --tests TimerTest jacocoTestReport
-# gradle test jacocoTestReport
